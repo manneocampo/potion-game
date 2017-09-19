a5f9cf3a8795944bf8d45c25073617a6b0d38d6c
Michelle Ocampo <manneocampo@gmail.com>
2017-09-18 23:16:44 -0400

    added btns and started js

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+/*firstNumber= random # selector to display first, 
+player has to match to this (id="random-number-selector")
+secondNumber=second random # selector for the buttons (id="crystals-random-number-selector")
+result=total-score (id="total-score") 
+winCounter id is win-counter
+lossesCounter id is losses-counter*/
+
+$(document).ready(function() {
+var firstNumber; 
+var secondNumber;
+var result; 
+var winCounter; 
+var lossesCounter;
+
+}
