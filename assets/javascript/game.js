8b627cb461b14e59322471a652882d86261697aa
Michelle Ocampo <manneocampo@gmail.com>
2017-09-21 00:28:37 -0400

    added some js and html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,15 +5,42 @@ result=total-score (id="total-score")
 winCounter id is win-counter
 lossesCounter id is losses-counter*/
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-//global variables 
-var firstNumber; 
-var secondNumber;
-var result; 
-var winCounter; 
-var lossesCounter;
-
-
-
+// //global variables 
+// var firstNumber; 
+// var secondNumber;
+// var result; 
+// var winCounter; 
+// var lossesCounter;
+// }
+var crystalGame = {
+		btnOne: 0, 
+		btnTwo: 0,
+		btnThree: 0,
+		btnFour:0,
+		firstNum: 0, 
+		totalNum:0,
+		totalWins:0,
+		totalLosses:0,
+		reset: function() {
+			this.btnOne=Math.ceil(Math.random()*12);
+			console.log(this.btnOne);
+			this.btnTwo=Math.ceil(Math.random()*12);
+			console.log(this.btnTwo);
+			this.btnThree=Math.ceil(Math.random()*12);
+			console.log(this.btnThree);
+			this.btnFour=Math.ceil(Math.random()*12);
+			console.log(this.btnFour);
+			this.firstNum=Math.ceil(Math.random()* (120 - 19 + 1)) + 19;
+			$("#random-number-selector").html('<div class="panel panel-default text-center" id="random-number-selector">' + firstNum + '</div>')
+			console.log(this.firstNum);
+			this.totalNum=0;
+			$("#total-score").html('<div class="panel-body text-center" id="total-score">' + totalNum + '</div>');
+			console.log(total-score);
+		}
+		gamePlay: function() {
+			this.reset();
+			
+		}
 }
